39d2234e660122da192d5853044212ff95f469f6
Julian H <cjulianh46@gmail.com>
2020-10-22 20:50:45 -0600

    Register page UI changes

--- src/translations.ts
+++ src/translations.ts
@@ -1,49 +1,56 @@
-import { FormField } from '@oryd/kratos-client'
+import { FormField } from '@oryd/kratos-client';
 
 const translations = {
   password: {
-    title: 'Password',
+    title: 'Contraseña',
     position: 2,
   },
   'traits.email': {
-    title: 'E-Mail',
+    title: 'Correo Electrónico',
     position: 1,
   },
+  'traits.name.first': {
+    title: 'Nombre',
+    position: 2,
+  },
+  'traits.name.last': {
+    title: 'Apellido',
+    position: 3,
+  },
+  'traits.website': {
+    title: 'Website',
+    position: 4,
+  },
   identifier: {
-    title: 'E-Mail',
+    title: 'Correo Electrónico',
     position: 0,
   },
   to_verify: {
-    title: 'Your email address',
+    title: 'Tu Correo Electrónico',
     position: 0,
   },
-}
+};
 
-type Translations = typeof translations
+type Translations = typeof translations;
 
-const getTitle = (key: string): string =>
-  key in translations ? translations[key as keyof Translations].title : key
+export const getTitle = (key: string): string =>
+  key in translations ? translations[key as keyof Translations].title : key;
 
-const getPosition = (field: FormField) =>
+export const getPosition = (field: FormField) =>
   field.name && field.name in translations
     ? translations[field.name as keyof Translations].position
-    : Infinity
+    : Infinity;
 
-const sortFormFields = (first: FormField, second: FormField) =>
-  getPosition(first) - getPosition(second)
-
-// this helper function translates the html input type to the corresponding partial name
-const toFormInputPartialName = (type: string) => {
+// This helper function translates the html input type to the corresponding partial name.
+export const toFormInputPartialName = (type: string) => {
   switch (type) {
     case 'hidden':
-      return 'form_input_hidden'
+      return 'form_input_hidden';
     case 'password':
-      return 'form_input_password'
+      return 'form_input_password';
     case 'submit':
-      return 'form_field_button'
+      return 'form_field_button';
     default:
-      return 'form_input_default'
+      return 'form_input_default';
   }
-}
-
-export { getTitle, sortFormFields, toFormInputPartialName }
+};
